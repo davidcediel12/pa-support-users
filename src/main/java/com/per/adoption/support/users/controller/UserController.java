781566b29e1a2ac92959ec53d0448f48275d61e5
David Cediel <esdago18@gmail.com>
2024-08-30 19:54:24 +0200

    Use the service to create the user in the controller

--- src/main/java/com/per/adoption/support/users/controller/UserController.java
+++ src/main/java/com/per/adoption/support/users/controller/UserController.java
@@ -3,6 +3,9 @@ package com.per.adoption.support.users.controller;
 
 import com.per.adoption.support.users.dto.CreatedUser;
 import com.per.adoption.support.users.dto.UserRequest;
+import com.per.adoption.support.users.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +16,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+
+    private final UserService userService;
 
 
     @PostMapping
@@ -21,18 +28,18 @@ public class UserController {
             @RequestHeader Map<String, String> headers,
             @RequestBody UserRequest userRequest) {
 
-        System.out.println(headers);
-        System.out.println(userRequest);
+        log.info("Received new request to save user");
+
+        log.debug("Headers {}", headers);
+        log.debug("Body {}", userRequest);
+
+        CreatedUser createdUser = userService.saveUser(userRequest);
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.put(HttpHeaders.LOCATION, List.of("1092"));
+        httpHeaders.put(HttpHeaders.LOCATION, List.of(createdUser.getId().toString()));
+
         return ResponseEntity.ok()
                 .headers(httpHeaders)
-                .body(
-                        CreatedUser.builder()
-                                .id(83L)
-                                .createdAt(OffsetDateTime.now())
-                                .build()
-                );
+                .body(createdUser);
     }
 }
