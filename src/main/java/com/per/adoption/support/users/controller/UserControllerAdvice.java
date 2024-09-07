a44982721613d164f30a1f0b01bee3744cc78ca3
David Cediel <esdago18@gmail.com>
2024-09-07 16:39:55 +0200

    Handle wrong parameters exception

--- src/main/java/com/per/adoption/support/users/controller/UserControllerAdvice.java
+++ src/main/java/com/per/adoption/support/users/controller/UserControllerAdvice.java
@@ -6,12 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.per.adoption.support.users.util.ErrorConstants.WRONG_PARAMETERS;
 
 @RestControllerAdvice
 @Slf4j
@@ -23,7 +26,7 @@ public class UserControllerAdvice {
         return new ResponseEntity<>(e.getError(), e.getHttpStatusCode());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(DataIntegrityViolationException.class)
     private ResponseEntity<Error> handleDataIntegrityViolation(final DataIntegrityViolationException e) {
 
         log.error("Error while interacting with the database {}", e.getMessage());
@@ -43,5 +46,19 @@ public class UserControllerAdvice {
         return new ResponseEntity<>(
                 new Error("SP-99", errorMessage, List.of(e.getMessage())),
                 HttpStatus.CONFLICT);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<Error> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        List<String> errorMessages = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ":" +
+                        fieldError.getDefaultMessage())
+                .toList();
+
+
+        return ResponseEntity.badRequest()
+                .body(new Error(WRONG_PARAMETERS.code(), WRONG_PARAMETERS.message(), errorMessages));
     }
 }
