92967a788f867e2463a13d20163fca33c68098fb
David Cediel <esdago18@gmail.com>
2024-09-08 13:28:00 +0200

    Add test to verify that the service save the user

--- src/test/java/com/per/adoption/support/users/service/impl/UserServiceImplTest.java
+++ src/test/java/com/per/adoption/support/users/service/impl/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.per.adoption.support.users.service.impl;
 
 import com.per.adoption.support.users.exception.ApiException;
 import com.per.adoption.support.users.mapper.UserMapper;
+import com.per.adoption.support.users.model.User;
 import com.per.adoption.support.users.repository.UserRepository;
 import com.per.adoption.support.users.repository.UserRoleRepository;
 import org.junit.jupiter.api.Test;
@@ -13,11 +14,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static com.per.adoption.support.users.util.Constants.USER_REQUEST;
+import static com.per.adoption.support.users.util.Constants.USER_ROLE;
 import static com.per.adoption.support.users.util.ErrorConstants.ROLE_DOES_NOT_EXISTS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -43,6 +46,16 @@ class UserServiceImplTest {
 
         assertEquals(ROLE_DOES_NOT_EXISTS, apiException.getError());
 
+    }
+
+    @Test
+    void shouldSaveUser() {
+        when(userRoleRepository.findByName(anyString())).thenReturn(Optional.of(USER_ROLE));
+        when(userMapper.requestToEntity(USER_REQUEST)).thenReturn(new User());
+
+        userService.saveUser(USER_REQUEST);
+
+        verify(userRepository, times(1)).save(any());
     }
 
 
--- src/test/java/com/per/adoption/support/users/util/Constants.java
+++ src/test/java/com/per/adoption/support/users/util/Constants.java
@@ -1,13 +1,17 @@
 package com.per.adoption.support.users.util;
 
 import com.per.adoption.support.users.dto.UserRequest;
+import com.per.adoption.support.users.model.UserRole;
 
 public class Constants {
     private Constants() {
     }
 
+    private static final String USER_ROLE_NAME = "USER";
+
+    public static final UserRole USER_ROLE = new UserRole(1, USER_ROLE_NAME, null);
 
     public static final UserRequest USER_REQUEST = new UserRequest(
             "115de516-ae8a-4db5-b63b-969c6696a848",
-            "USER123", "user@gmail.com", "Spain", "USER", "2092");
+            "USER123", "user@gmail.com", "Spain", USER_ROLE_NAME, "2092");
 }
